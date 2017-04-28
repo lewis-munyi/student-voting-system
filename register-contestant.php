65eb90f5e0c81381bdd7a91f5323cb168342380c
lewis-munyi <lewismunyi97@gmail.com>
2017-04-28 09:54:29 +0300

    Concanteneted name, Edited table added null values to vote and course

--- register-contestant.php
+++ register-contestant.php
@@ -1,57 +1,37 @@
 <?php
-echo "Hello world!";
+// Get data from form
 $reg_no = $_POST['reg_no'];
-$name = $_POST['name'];
-$course_code = $_POST['course_code'];
+$fname = $_POST['fname'];
+$sname = $_POST['sname'];
+$reg_no = $_POST['reg_no'];
 $course_title = $_POST['course_title'];
 $course_year = $_POST['course_year'];
 $email = $_POST['email'];
 
-echo $course_year;
-echo ($reg_no.$course_year.$course_title.$course_code.$email.$name);
+$contestant_name = ($fname." ".$sname);
 
+// Database data
 $db_host = "localhost";
 $db_name = "votingSystem";
 $db_user = "root";
 $db_password ="root";	
 
-/*$connection = msql_connect($db_host, $db_user ,$db_password, $db_name) or die("Error establishing connection!");
-
-mysql_query($connection,"INSERT INTO nominees (nomineeRegistrationNumber, nomineeName, nomineeCourseCode, nomineeCourse, nomineeYear, nomineeEmail)VALUES('$reg_no', '$name', '$course_code', '$course_title', '$course_year', '$email')") or die("Error populating database");*/
-/*try {
-	$connection = msql_connect($localhost, $db_user ,$db_password, $db_name);
-	try {
-		mysql_query($connection,"INSERT INTO nominees (nomineeRegistrationNumber, nomineeName, nomineeCourseCode, nomineeCourse, nomineeEmail)VALUES ($reg_no, $name, $course_code, $course_title, $course_year, $email)";
-	} catch (Exception $e) {
-		echo "Error populating database";
-	}
-} catch (Exception $e) {
-	echo "Error establishing connection!";
-}
-*/
-/*echo ($name);
-echo "Hello world!";*/
-
-
-
 // Create connection
-$conn = mysqli_connect($db_host, $db_user ,$db_password, $db_name);
+$connection = mysqli_connect($db_host, $db_user ,$db_password, $db_name);
 // Check connection
-if (!$conn) {
+if (!$connection) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-$sql = "INSERT INTO contestant(contestantName, contestantRegistrationNumber, contestantCourseCode, contestantCourse, contestantYear, contestantEmail)
-VALUES ('$name', '$reg_no', '$course_code', '$course_title', '$course_year', '$email')";
+// Sql Query
+$sql = "INSERT INTO contestants(contestantName, contestantRegistrationNumber, contestantCourse, academic_year, contestantEmail)
+VALUES ('$contestant_name', '$reg_no', '$course_title', '$course_year', '$email')";
 
-if (mysqli_query($conn, $sql)) {
+if (mysqli_query($connection, $sql)) {
     echo "New record created successfully";
 } else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    echo "Error: " . $sql . "<br>" . mysqli_error($connection);
 }
-
-mysqli_close($conn);
-
-
-
+// Close connection
+mysqli_close($connection);
 ?>
